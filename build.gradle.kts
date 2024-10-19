a0ee8e4486bcdb582b245c6063564ec46d43b329
MrMasrozYTLIVE <61359286+MrMasrozYTLIVE@users.noreply.github.com>
2024-10-19 21:57:47 +0300

    Fixed few issues since Gradle KTS is something new to me

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,9 @@ group = "net.mitask"
 version = "1.0-SNAPSHOT"
 
 java {
-    toolchain.languageVersion = JavaLanguageVersion.of(17)
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
 
 repositories {
@@ -14,10 +16,10 @@ repositories {
 }
 
 dependencies {
-    compileOnly 'org.projectlombok:lombok:1.18.34'
-    annotationProcessor 'org.projectlombok:lombok:1.18.34'
+    compileOnly("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
 
-    implementation 'com.google.code.gson:gson:2.11.0'
-    implementation 'org.yaml:snakeyaml:2.3'
-    implementation 'com.google.auto.service:auto-service:1.1.1'
+    implementation("com.google.code.gson:gson:2.11.0")
+    implementation("org.yaml:snakeyaml:2.3")
+    implementation("com.google.auto.service:auto-service:1.1.1")
 }
