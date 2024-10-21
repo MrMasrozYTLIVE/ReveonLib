e77fb42603f4e098d69abecb2637763c0f43d1df
MrMasrozYTLIVE <61359286+MrMasrozYTLIVE@users.noreply.github.com>
2024-10-21 19:28:58 +0300

    After wasting almost 2 hours I couldnt get auto saving to work so for now I'll get rid of it

--- src/main/java/net/mitask/reveonlib/config/ConfigLoader.java
+++ src/main/java/net/mitask/reveonlib/config/ConfigLoader.java
@@ -1,14 +1,11 @@
 package net.mitask.reveonlib.config;
 
-import net.bytebuddy.ByteBuddy;
-import net.bytebuddy.implementation.MethodDelegation;
-import net.bytebuddy.matcher.ElementMatchers;
-import net.mitask.reveonlib.config.annotations.AutoSave;
 import net.mitask.reveonlib.config.annotations.Config;
 import net.mitask.reveonlib.config.impl.JsonConfig;
 import net.mitask.reveonlib.config.impl.YamlConfig;
 
 import java.io.File;
+import java.nio.file.Paths;
 
 @SuppressWarnings("unused")
 public class ConfigLoader {
@@ -21,10 +18,7 @@ public class ConfigLoader {
         String name = configMetadata.name();
         String path = configMetadata.path();
         Config.FormatType format = configMetadata.format();
-        File configFile = new File(path, name + "." + format.name().toLowerCase());
-
-        AutoSave autoSaveAnnotation = configClass.getAnnotation(AutoSave.class);
-        boolean autoSave = autoSaveAnnotation != null && autoSaveAnnotation.value();
+        File configFile = new File(Paths.get(".").toAbsolutePath() + path, name + "." + format.name().toLowerCase());
 
         AbstractConfig<T> configHandler;
         if (format == Config.FormatType.JSON) {
@@ -33,33 +27,7 @@ public class ConfigLoader {
             configHandler = new YamlConfig<>(configFile, configClass);
         }
 
-        // Automatically load configuration data
-        T configInstance = configHandler.load();
-
-        // Create a proxy to intercept field modifications for auto-saving
-        try {
-            return new ConfigWrapper<>(createAutoSaveProxy(configInstance, configHandler, autoSave), configHandler);
-        } catch (InstantiationException | IllegalAccessException e) {
-            throw new RuntimeException("Failed to create Config!", e);
-        }
-    }
-
-    public static <T> T createAutoSaveProxy(T configInstance, AbstractConfig<T> configHandler, boolean autoSave) throws InstantiationException, IllegalAccessException {
-        var configClass = configInstance.getClass();
-        return (T) new ByteBuddy()
-                .subclass(configClass)
-                .method(ElementMatchers.nameStartsWith("set"))
-                .intercept(MethodDelegation.to(new Interceptor<>(configInstance, configHandler, autoSave)))
-                .make()
-                .load(configClass.getClassLoader())
-                .getLoaded()
-                .newInstance();
-    }
-
-    private record Interceptor<T>(T configInstance, AbstractConfig<T> configHandler, boolean autoSave) {
-        public void intercept() throws Throwable {
-            if(autoSave) configHandler.save(configInstance);
-        }
+        return new ConfigWrapper<>(configHandler.load(), configHandler);
     }
 }
 
--- src/main/java/net/mitask/reveonlib/config/annotations/AutoSave.java
+++
@@ -1,9 +0,0 @@
-package net.mitask.reveonlib.config.annotations;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface AutoSave {
-    boolean value() default true;
-}
